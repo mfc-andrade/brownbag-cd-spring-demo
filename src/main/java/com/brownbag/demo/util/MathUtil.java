b171049865763ea7f69cdc3e5e8a08aca8dae34f
Miguel Andrade <miguel.andrade@cross-join.com>
2023-01-05 10:22:46 +0000

    added sample controller and sample tests

---
+++ src/main/java/com/brownbag/demo/util/MathUtil.java
@@ -0,0 +1,9 @@
+package com.brownbag.demo.util;
+
+public class MathUtil {
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+}
--- src/test/java/com/brownbag/demo/DemoApplicationTests.java
+++ src/test/java/com/brownbag/demo/DemoApplicationTests.java
@@ -1,13 +1,23 @@
 package com.brownbag.demo;
 
+import com.brownbag.demo.util.MathUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
-    void contextLoads() {
+    void shouldSumCorrectly() {
+        assertEquals(5, MathUtil.sum(2, 3));
+    }
+
+    @Test
+    void shouldSumIncorrectly() {
+        assertNotEquals(6, MathUtil.sum(5, 4));
     }
 
 }
