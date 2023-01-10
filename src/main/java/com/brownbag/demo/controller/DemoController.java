f1545927509d653166c7add127d28b06acfa4928
Miguel Andrade <miguel.andrade@cross-join.com>
2023-01-10 14:11:19 +0000

    endpoints

--- src/main/java/com/brownbag/demo/controller/DemoController.java
+++ src/main/java/com/brownbag/demo/controller/DemoController.java
@@ -3,7 +3,7 @@ package com.brownbag.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("")
 public class DemoController {
 
     @GetMapping("/demo")
@@ -11,7 +11,7 @@ public class DemoController {
         return "Demo String";
     }
 
-    @GetMapping
+    @GetMapping("/")
     public String hello() {
         return "Hello World";
     }
