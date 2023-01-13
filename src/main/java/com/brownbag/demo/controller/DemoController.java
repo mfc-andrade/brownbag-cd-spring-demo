25e2c749893016fdd5a51c6a6627d89f1c0fc24e
Miguel Andrade <miguel.andrade@cross-join.com>
2023-01-13 10:28:40 +0000

    update

--- src/main/java/com/brownbag/demo/controller/DemoController.java
+++ src/main/java/com/brownbag/demo/controller/DemoController.java
@@ -3,7 +3,7 @@ package com.brownbag.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("")
+@RestController()
 public class DemoController {
 
     @GetMapping("/demo")
@@ -13,7 +13,7 @@ public class DemoController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello World";
+        return "Hello World 2";
     }
 
 }
